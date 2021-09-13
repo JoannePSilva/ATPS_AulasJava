17f1dd267c68da250bdd3331f205bfec24d07b4d
Joanne PSilva <joannepassos@gmail.com>
2021-09-13 15:52:17 -0300

    Ativ Aula 3

---
+++ Products.java
@@ -0,0 +1,27 @@
+package modulo1.aula3;
+
+import java.util.Scanner;
+
+public class Products {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=========== Dark Store ============");
+        System.out.println("Digite o produto que deseja cadastrar");
+        String produto = scanner.next();
+        System.out.println("Cor:");
+        String cor = scanner.next();
+        System.out.println("Tamanho:");
+        String tam = scanner.next();
+
+        System.out.println("Digite o valor do produto");
+        double preco = scanner.nextDouble();
+
+        System.out.println("=========== Cadastro realizado com sucesso! ============");
+        System.out.printf("\t O produto cadastrado:  %s, \n cor: %s \n tamanho: %s \n preço: f%\n",produto, cor, tam, preco);
+        System.out.println("Deseja continuar cadastrando?");
+    }
+
+
+    }
+
