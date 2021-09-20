3e1e656b5523348f90e73affcdcfa3fee6912773
Joanne PSilva <joannepassos@gmail.com>
2021-09-20 10:58:19 -0300

    Aula7 - impressão do números pares até 100

---
+++ src/Aula7/Aula7_numerosPares_For.java
@@ -0,0 +1,13 @@
+package Aula7;
+
+public class Aula7_numerosPares_For {
+    public static void main(String[] args) {
+
+        int num = 100;
+        for (int i = 0; i <= num ; i += 2) {
+            System.out.println(i);
+
+            }
+
+        }
+    }
