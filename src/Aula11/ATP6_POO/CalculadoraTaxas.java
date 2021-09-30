644c625e728c5798a2412d40aed0c26660639889
Joanne PSilva <joannepassos@gmail.com>
2021-09-30 14:18:11 -0300

    Início switch case

--- src/Aula11/ATP6_POO/CalculadoraTaxas.java
+++ src/Aula11/ATP6_POO/CalculadoraTaxas.java
@@ -1,4 +1,26 @@
-package Aula11.OrientacaoObjetos.ATP6_POO;
+package Aula11.ATP6_POO;
+
+import java.util.Scanner;
 
 public class CalculadoraTaxas {
+
+
+        double valor_conta = 100.00;
+
+    public double taxaTransferido(double valor, double valor1) {
+        double taxa_transfer = valor_conta * 0.001;
+        return taxa_transfer;
+    }
+
+    public void taxaSaque() {
+        int taxa_cont = 0;
+        if (taxa_cont > 5) {
+            double taxa_saque = valor_conta * 1.3;
+            taxa_cont++;
+
+        }
+    }
+    public void imprimir_valores(){
+        System.out.println("Valor na conta: %d| Valor com taxa de transferência: %f | Valor com taxa se saque %.2f");
+    }
 }
--- src/Aula11/ATP6_POO/Main.java
+++ src/Aula11/ATP6_POO/Main.java
@@ -1,4 +1,42 @@
-package Aula11.OrientacaoObjetos.ATP6_POO;
+package Aula11.ATP6_POO;
+
+import Aula11.Calculadora;
+
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("====CALCULADORA DE TAXAS=====");
+        System.out.println( "Escolha a operação que deseja realizar:\n 1 - Depositar \n 2 - Sacar \n 3 -Transferir \n 4- Sair");
+        int opcao = scanner.nextInt();
+
+        switch (opcao){
+
+            case 1:
+                System.out.println("Ainda vamos implementar :0)");
+                break;
+
+            case 2:
+                System.out.println("Ainda vamos implementar :0)");
+                break;
+
+            case 3:
+                System.out.println("Ainda vamos implementar :0)");
+                break;
+
+            case 4:
+                System.out.println("Até breve!");
+                break;
+        }
+
+
+
+        CalculadoraTaxas calculadoraTaxas = new CalculadoraTaxas();
+        calculadoraTaxas.taxaTransferido(10, 20);
+        calculadoraTaxas.taxaSaque();
+        calculadoraTaxas.valor_conta = 1000.00;
+    }
 }
