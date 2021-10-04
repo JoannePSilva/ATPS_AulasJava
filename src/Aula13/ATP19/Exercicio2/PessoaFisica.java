f380eed44affa5bbf50df1b752fe43accfcba2df
Joanne PSilva <joannepassos@gmail.com>
2021-10-04 17:40:07 -0300

    criação classe Pessoa Física com getters e setters

---
+++ src/Aula13/ATP19/Exercicio2/PessoaFisica.java
@@ -0,0 +1,62 @@
+package Aula13.ATP19.Exercicio2;
+
+public class PessoaFisica {
+
+    String nome;
+    String sobrenome;
+    String codigo;
+    String rg;
+    String cpf;
+    int idade;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+}
