f67056a75873fa67deba77b66bd088637e118fd5
Joanne PSilva <joannepassos@gmail.com>
2021-10-04 14:16:33 -0300

    Chamada dos métodos na TelaPrincipalBanco

--- src/Aula12/ATP18/TelaPrincipal_Banco.java
+++ src/Aula12/ATP18/TelaPrincipal_Banco.java
@@ -5,5 +5,17 @@ public class TelaPrincipal_Banco {
     public static void main(String[] args) {
 
         ContaCorrente cc = new ContaCorrente();
+
+        cc.setAgencia(1232);
+        int ag = cc.getAgencia();
+
+        cc.setNum_conta(123232987);
+        long cc1 = cc.getNum_conta();
+
+        cc.setCodigo_cliente("CDE987");
+        String cod_cliente = cc.getCodigo_cliente();
+
+        cc.setSaldo(12.345);
+        double saldo = cc.getSaldo();
     }
 }
