e47ee388a56f7b595938b929443cce3b68f4a166
Joanne PSilva <joannepassos@gmail.com>
2021-10-04 14:01:15 -0300

    Chamada dos métos e impressão em TelaPrincipal_Loja

--- src/Aula12/ATP18/Produto.java
+++ src/Aula12/ATP18/Produto.java
@@ -5,37 +5,44 @@ public class Produto {
     private String codigo;
     private String nome;
     private String descricao;
-    private String valor;
+    private double valor;
 
     public String getCodigo() {
         return codigo;
     }
 
     public void setCodigo(String codigo) {
+
         this.codigo = codigo;
     }
 
     public String getNome() {
+
         return nome;
     }
 
     public void setNome(String nome) {
+
         this.nome = nome;
     }
 
     public String getDescricao() {
+
         return descricao;
     }
 
     public void setDescricao(String descricao) {
+
         this.descricao = descricao;
     }
 
-    public String getValor() {
+    public double getValor() {
+
         return valor;
     }
 
-    public void setValor(String valor) {
+    public void setValor(double valor) {
+
         this.valor = valor;
     }
 
--- src/Aula12/ATP18/TelaPrincipal_Loja.java
+++ src/Aula12/ATP18/TelaPrincipal_Loja.java
@@ -6,6 +6,24 @@ public class TelaPrincipal_Loja {
 
         Produto produto = new Produto();
 
+
+        produto.setNome("Vestido");
+        String n1 = produto.getNome();
+        System.out.println(n1);
+
+        produto.setDescricao("Vestido preto");
+        String d1 = produto.getDescricao();
+        System.out.println(d1);
+
+        produto.setValor(89.99);
+        double v1 = produto.getValor();
+        System.out.println(v1);
+
+        produto.setCodigo("ABD123");
+        String p1 = produto.getCodigo();
+        System.out.println(p1);
+
+
     }
 
 
