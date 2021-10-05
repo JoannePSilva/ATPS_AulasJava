0fa9dde76c6b0b9e8fe1f526cdd21b747b156148
Joanne PSilva <joannepassos@gmail.com>
2021-10-05 15:50:01 -0300

    Classe coordenador recebe funcionário

---
+++ src/Aula14/ATP20/Coordenador.java
@@ -0,0 +1,24 @@
+package Aula14.ATP20;
+
+public class Coordenador extends Funcionario{
+
+    private String curso_coordenado;
+    private String bonus;
+
+    public String getCurso_coordenado() {
+        return curso_coordenado;
+    }
+
+    public void setCurso_coordenado(String curso_coordenado) {
+        this.curso_coordenado = curso_coordenado;
+    }
+
+    public String getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(String bonus) {
+        this.bonus = bonus;
+    }
+
+    }
