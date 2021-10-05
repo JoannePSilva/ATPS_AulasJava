51c3630876801ea1c6d4c7de4127add94e1b77a5
Joanne PSilva <joannepassos@gmail.com>
2021-10-05 15:39:50 -0300

    Getters and setters

--- src/Aula14/ATP20/Funcionario.java
+++ src/Aula14/ATP20/Funcionario.java
@@ -2,9 +2,33 @@ package Aula14.ATP20;
 
 public class Funcionario {
 
-
     private String curso;
     private String coordenador;
     private String bonus;
+
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getCoordenador() {
+        return coordenador;
+    }
+
+    public void setCoordenador(String coordenador) {
+        this.coordenador = coordenador;
+    }
+
+    public String getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(String bonus) {
+        this.bonus = bonus;
+    }
 
 }
