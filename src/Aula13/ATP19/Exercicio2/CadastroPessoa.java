4487c8838f79d1cb4a2c050950981021ef36e378
Joanne PSilva <joannepassos@gmail.com>
2021-10-05 16:54:25 -0300

    Impressão Funcionário, Coordenador, Aluno

--- src/Aula13/ATP19/Exercicio2/CadastroPessoa.java
+++ src/Aula13/ATP19/Exercicio2/CadastroPessoa.java
@@ -4,43 +4,36 @@ public class CadastroPessoa {
 
     public static void main(String[] args) {
         PessoaFisica pessoa = new PessoaFisica();
-        PessoaFisica [] pessoas = new PessoaFisica[5];
+        PessoaFisica [] pessoas = new PessoaFisica[3];
 
         PessoaFisica ps1 = new PessoaFisica();
         ps1.setNome("Joanne");
-        String nome = ps1.getNome();
         ps1.setSobrenome("Silva");
-        String sobrenome = ps1.getSobrenome();
         ps1.setCodigo("cbd 123");
-        String cod = ps1.getCodigo();
         ps1.setCpf("213.123.232");
-        String cpf = ps1.getCpf();
         ps1.setRg("456.456.543");
-        String rg = ps1.getRg();
+        pessoas [0] = ps1;
 
         PessoaFisica ps2 = new PessoaFisica();
         ps2.setNome("Julianne");
-        String nome1 = ps2.getNome();
         ps2.setSobrenome("Silva");
-        String sobrenome1 = ps2.getSobrenome();
         ps2.setCodigo("dfg 123");
-        String cod1 = ps2.getCodigo();
         ps2.setCpf("098.123.232");
-        String cpf1 = ps2.getCpf();
         ps2.setRg("765.456.543");
-        String rg1 = ps2.getRg();
+        pessoas [1] = ps2;
 
         PessoaFisica ps3 = new PessoaFisica();
         ps3.setNome("Suzanne");
-        String nome2 = ps2.getNome();
         ps3.setSobrenome("Passos");
-        String sobrenome2 = ps2.getSobrenome();
         ps3.setCodigo("ppf 123");
-        String cod2 = ps2.getCodigo();
         ps3.setCpf("432.123.232");
-        String cpf2 = ps2.getCpf();
         ps3.setRg("654.456.543");
-        String rg2 = ps2.getRg();
+        pessoas [2] = ps3;
+
+        for (PessoaFisica pf: pessoas) {
+            System.out.println(pf);
+
+        }
         }
 
     }
--- src/Aula14/ATP20/Universidade_Main.java
+++ src/Aula14/ATP20/Universidade_Main.java
@@ -7,8 +7,30 @@ public class Universidade_Main {
         Coordenador coordenador = new Coordenador();
         Aluno aluno = new Aluno();
 
+    funcionario.setNome("Gerônimo");
+    funcionario.setSobrenome("Ornellas");
+    funcionario.setDepartamento("História");
+    funcionario.setSalario(2.565);
+    funcionario.setMatricula("ABD123");
 
 
+    coordenador.setNome("Ferdinando");
+    coordenador.setSobrenome("O Touro");
+    coordenador.setCurso_coordenado("Artes");
+    coordenador.setDepartamento("Artístico");
+    coordenador.setSalario(5.456);
+    coordenador.setBonus("10%");
+
+    aluno.setNome("Joanne");
+    aluno.setSobrenome("Silva");
+    aluno.setCurso("Artes");
+    aluno.setMatricula("CDF234");
+    aluno.setTurma("v237");
+
+
+        System.out.printf("Funcionário: %s %s | Matéria: %s | Salário: %.4f  | Matrícula: %s", funcionario.getNome() , funcionario.getSobrenome(), funcionario.getDepartamento(), funcionario.getSalario(), funcionario.getMatricula());
+        System.out.printf("\nCoordenador: %s %s | Curso: %s | Departamento: %s | Salário: %.4f  | Bônus: %s", coordenador.getNome(), coordenador.getSobrenome(), coordenador.getCurso_coordenado(), coordenador.getDepartamento(), coordenador.getSalario(), coordenador.getBonus());
+        System.out.printf("\nAluno: %s %s | Curso: %s | Matrícula: %s | Turma: %s ", aluno.getNome(), aluno.getSobrenome(), aluno.getCurso(), aluno.getMatricula(), aluno.getTurma() );
 
     }
 }
