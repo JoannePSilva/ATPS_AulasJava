8170754ad0e840d7b9159a5ccc0138bcdb9e3db8
Joanne PSilva <joannepassos@gmail.com>
2021-10-05 15:54:55 -0300

    início método main

---
+++ src/Aula14/ATP20/Universidade_Main.java
@@ -0,0 +1,14 @@
+package Aula14.ATP20;
+
+public class Universidade_Main {
+
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        Coordenador coordenador = new Coordenador();
+        Aluno aluno = new Aluno();
+
+
+
+
+    }
+}
