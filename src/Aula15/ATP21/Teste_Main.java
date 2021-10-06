29b18ed7e8fdbb4c15af951dd2b3b491f5bba2f4
Joanne PSilva <joannepassos@gmail.com>
2021-10-06 13:52:38 -0300

    criação do main

--- src/Aula15/ATP21/Teste_Main.java
+++ src/Aula15/ATP21/Teste_Main.java
@@ -1,4 +1,8 @@
 package Aula15.ATP21;
 
 public class Teste_Main {
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+        ContaCorrente cc = new ContaCorrente();
+    }
 }
