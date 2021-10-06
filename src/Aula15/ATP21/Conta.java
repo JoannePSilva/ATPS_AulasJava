b9e630e7586684fd2ef64bf45fecf59d5cfdf894
Joanne PSilva <joannepassos@gmail.com>
2021-10-06 13:39:10 -0300

    Calsse Conta com getters e setters

---
+++ src/Aula15/ATP21/Conta.java
@@ -0,0 +1,26 @@
+package Aula15.ATP21;
+
+public class Conta {
+
+    private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getClient_code() {
+        return client_code;
+    }
+
+    public void setClient_code(String client_code) {
+        this.client_code = client_code;
+    }
+
+    private String client_code;
+
+
+}
