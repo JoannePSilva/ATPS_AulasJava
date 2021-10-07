dcc9cd56226a53bb6f310c47ac39f791e777d29e
Joanne PSilva <joannepassos@gmail.com>
2021-10-07 16:23:47 -0300

    Classe Aluno com getters e setters + Tela

---
+++ src/Aula16/ATP22/Aluno.java
@@ -0,0 +1,45 @@
+package Aula16.ATP22;
+
+public class Aluno {
+
+    private String nome;
+    private String sobrenome;
+    private int idade;
+    private String matricula;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+
+
+}
---
+++ src/Aula16/ATP22/Tela.java
@@ -0,0 +1,25 @@
+package Aula16.ATP22;
+
+public class Tela {
+
+        public static void main(String[] args) {
+
+            Aluno a = new Aluno();
+            Aluno a1 = new Aluno();
+
+            a.setNome("Joanne");
+            a.setSobrenome("Silva");
+            a.setIdade(34);
+            a.setMatricula("abc123");
+
+            a1.setNome("Julius");
+            a1.setSobrenome("Julio");
+            a1.setIdade(124);
+            a1.setMatricula("cde456");
+
+
+        }
+
+
+    }
+}
