a4f1528326e391e625e99c43749b3c0150d31d6c
Joanne PSilva <joannepassos@gmail.com>
2021-10-25 13:16:45 -0300

    Impressão, main, tratamento de exceções

--- src/Aula27/ATP30/Model/Operacoes.java
+++ src/Aula27/ATP30/Model/Operacoes.java
@@ -19,27 +19,4 @@ public class Operacoes {
         int resultado = num1 / num2;
         return resultado;
     }
-
-    public void escolha_menu(int num_digitado) {
-        switch (num_digitado) {
-            case 1:
-                int resultado = soma(2, 3);
-                System.out.printf("Você escolheu adição\n O resultado da soma é: " + resultado);
-                break;
-            case 2:
-                int resultado_sub = subtracao(2, 3);
-                System.out.printf("Você escolheu subtração\n O resultado da subtração: " + resultado_sub);
-                break;
-            case 3:
-                int resultado_mult = multiplicacao(2, 3);
-                System.out.printf("Você escolheu multiplicação\n O resultado da multiplicação é: " + resultado_mult);
-                break;
-            case 4:
-                int resultado_div = divisao(2, 3);
-                System.out.printf("Você escolheu divisão\n O resultado da divisão é: " + resultado_div);
-                break;
-
-        }
-    }
-
 }
--- src/Aula27/ATP30/View/Main.java
+++ src/Aula27/ATP30/View/Main.java
@@ -1,20 +1,10 @@
 package Aula27.ATP30.View;
 
-import java.util.Scanner;
+import Aula27.ATP30.Controller.OperacoesController;
 
 public class Main {
     public static void main(String[] args) {
-
-        
-        menu();
+        OperacoesController oc = new OperacoesController();
+        oc.escolha_menu();
     }
-
-    public static int menu() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o número da operação que deseja realizar:\n 1 - Adição\n 2 - Subtração\n 3 - Multiplicação\n4 - Divisão\n 0 - Sair");
-        int nume_digitado = Integer.parseInt(scanner.nextLine());
-        return nume_digitado;
-    }
-    
-    
 }
