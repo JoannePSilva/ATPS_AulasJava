cbe498d440fc510d6aff7637b34985ed9a3bc53b
Joanne PSilva <joannepassos@gmail.com>
2021-11-12 16:16:05 -0300

    Inicio ATP49

---
+++ src/Aula36/ATP49/Views/View.java
@@ -0,0 +1,33 @@
+package Aula36.ATP49.Views;
+//Altere a classe usada na 47 para prepareStatement
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class View{
+    public static void main(String[] args) {
+        try {
+
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "2609");
+            PreparedStatement prepStatement = conn.prepareStatement("SELECT * FROM categoria");
+
+            prepStatement.execute();
+            ResultSet ids = prepStatement.getResultSet();
+
+            //Impressão
+            while(ids.next()){
+                int id = ids.getInt("id");
+                String nome = ids.getString("nome");
+                System.out.printf("ID: %d | Nome: %s \n", id, nome);
+                System.out.println("--------------------------------");
+            }
+
+            conn.close(); //fechar conexão
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Aula36/ATP49/Views/View1.java
@@ -0,0 +1,38 @@
+package Aula36.ATP49.Views;
+//Receber parâmetros - nome e descrição através de variáveis e troca do Statement para preparedStatemente
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class View1 {
+    public static void main(String[] args) {
+        try {
+            String nome = "Caribe";
+            String descricao = "Praias";
+
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "2609");
+
+            PreparedStatement prepStatement = conn.prepareStatement("insert into categoria(nome, descricao)values(?,?)", Statement.RETURN_GENERATED_KEYS);
+
+            prepStatement.setString(1, nome);
+            prepStatement.setString(2, descricao);
+
+            prepStatement.execute();
+            ResultSet ids = prepStatement.getGeneratedKeys();
+
+
+            while(ids.next()){
+                int id = ids.getInt(1);
+                System.out.println("Categoria com id " + id + " inserido com sucesso!");
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
