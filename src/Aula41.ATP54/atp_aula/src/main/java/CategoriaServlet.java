8959bb5b35603d7f56e2d8e0f10a7bba8619134d
Joanne PSilva <joannepassos@gmail.com>
2021-11-22 16:01:23 -0300

    Atividade 54

---
+++ src/Aula41.ATP54/atp_aula/src/main/java/CategoriaServlet.java
@@ -0,0 +1,24 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = "/categoria")
+public class CategoriaServlet extends HttpServlet{
+    
+    @Override //req = requisição - solicitação do usuário
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      String nome = req.getParameter("nome");
+
+        //response - resposta do servidor ao cliente
+        PrintWriter out = resp.getWriter();
+        out.printf(" Categoria  -- Nome = %s", nome);
+    }
+}
---
+++ src/Aula41.ATP54/atp_aula/target/atp_aula-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <h2>Hello World!</h2>
+        <a href="/atp_aula-1/categoria">Categoria</a>
+        <a href="/atp_aula-1/produto">Categoria</a>
+    </body>
+</html>
