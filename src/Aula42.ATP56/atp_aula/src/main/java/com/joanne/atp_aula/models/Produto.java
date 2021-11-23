065b19532d37c153b5856df9db0479e38f835ea1
Joanne PSilva <joannepassos@gmail.com>
2021-11-23 14:04:20 -0300

    Aula ATP55 E

---
+++ src/Aula42.ATP56/atp_aula/src/main/java/com/joanne/atp_aula/models/Produto.java
@@ -0,0 +1,30 @@
+package com.joanne.atp_aula.models;
+
+import javax.xml.validation.Validator;
+
+public class Produto {
+    
+    private String nome;
+    private float valor;
+    private int idCategoria;
+   
+   
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public float getValor() {
+        return valor;
+    }
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+}
