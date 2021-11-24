5d515178f0f9d6cbe2286adc7db8a79a85db817f
Joanne PSilva <joannepassos@gmail.com>
2021-11-24 15:54:52 -0300

    ATP 57 - JSP, servlets, JAVA WEB

--- src/Aula43.ATP57/atp_aula/src/main/java/com/joanne/atp_aula/servlets/CategoriaReadServlet.java
+++ src/Aula43.ATP57/atp_aula/src/main/java/com/joanne/atp_aula/servlets/CategoriaReadServlet.java
@@ -1,11 +1,11 @@
 package com.joanne.atp_aula.servlets;
 import java.io.IOException;
-import java.io.PrintWriter;
-
+import java.util.ArrayList;
 
 import com.joanne.atp_aula.models.Categoria;
 import com.joanne.atp_aula.dao.CategoriaDao;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -16,10 +16,14 @@ import jakarta.servlet.http.HttpServletResponse;
 public class CategoriaReadServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
         CategoriaDao dao =  new CategoriaDao();
-        for (Categoria model : dao.read()) {
-            out.printf("%d - %s \n", model.getId(), model.getNome());
+        
+        ArrayList<Categoria> categorias = dao.read();
+        req.setAttribute("categoria", categorias);
+        //for (Categoria model : dao.read()) {
+          //  out.printf("%d - %s \n", model.getId(), model.getNome());
+
+          RequestDispatcher rd = req.getRequestDispatcher("/categoria-lista.jsp");
+          rd.forward(req, resp);
         }
     }
-}
