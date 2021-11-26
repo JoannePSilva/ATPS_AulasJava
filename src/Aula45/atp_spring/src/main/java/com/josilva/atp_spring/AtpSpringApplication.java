f39b410b6c3f43555ca7dc06c57ce85f9cf98192
Joanne PSilva <joannepassos@gmail.com>
2021-11-26 14:19:32 -0300

    Aula 45 - Spring config

---
+++ src/Aula45/atp_spring/src/main/java/com/josilva/atp_spring/AtpSpringApplication.java
@@ -0,0 +1,13 @@
+package com.josilva.atp_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtpSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtpSpringApplication.class, args);
+	}
+
+}
